46050168865f762094cf5eed253b576960c61fae
TiagoUniverse <tiagocesar68@gmail.com>
2024-01-15 14:35:52 -0300

    Aula 7: Eloquent e Model

--- example-app/app/Models/Cliente.php
+++ example-app/app/Models/Cliente.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+
+    // Essas duas variáveis são caso você deseje personalizar como o laravel
+    // vai trabalhar com esse Model.
+    protected $table = 'clientes_ativos';
+
+    protected $primaryKey = 'codigo';
+
+    // Permite que esses campos possam ser preenchidos em um []
+    protected $fillable = [
+        'nome',
+        'endereco',
+        'numero'
+    ];
+
 }
+ 
