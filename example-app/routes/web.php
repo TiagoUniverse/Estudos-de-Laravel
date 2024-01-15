f0291b14c7ccad90a9821068a4d127576a03b4c2
TiagoUniverse <tiagocesar68@gmail.com>
2024-01-15 17:17:21 -0300

    Personalizando a rota inicial

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,13 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('inicio');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
